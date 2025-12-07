f5752029d57b9218143baafee42a765bb2d137f3
taylanmayckon <eusoutaylann@gmail.com>
2025-12-07 02:41:00 -0300

    Erros

--- pokemons.pl
+++ pokemons.pl
@@ -1,12 +1,12 @@
 %Meowscarada: Planta / Sombrio
 %Wiglytuff: Normal / Fada
-%Swampert: Água / Terra
+%Swampert: agua / Terra
 %Scizor: Inseto / Aço
 %Dragonite: Dragão / Voador
-%Jynx: Gelo / Psíquico
+%Jynx: Gelo / psiquico
 %Hisuian Arcanine: Fogo / Pedra
 %Gengar: Fantasma / Venenoso
-%Pawmot: Elétrico / Lutador
+%Pawmot: eletrico / Lutador
 
 
 % Informações do pokemon
@@ -26,12 +26,12 @@ info(147, 'dratini', 'dragao', 'nenhum', 41).
 info(148, 'dragonair', 'dragao', 'nenhum', 61).
 info(149, 'dragonite', 'dragao', 'voador', 91).
 % Linha Mudkip
-info(258, 'mudkip', 'água', 'nenhum', 50).
-info(259, 'marshtomp', 'água', 'terra', 70).
-info(260, 'swampert', 'água', 'terra', 100).
+info(258, 'mudkip', 'agua', 'nenhum', 50).
+info(259, 'marshtomp', 'agua', 'terra', 70).
+info(260, 'swampert', 'agua', 'terra', 100).
 % Linha Jynx 
-info(238, 'smoochum', 'gelo', 'psíquico', 45).
-info(124, 'jynx', 'gelo', 'psíquico', 65).
+info(238, 'smoochum', 'gelo', 'psiquico', 45).
+info(124, 'jynx', 'gelo', 'psiquico', 65).
 % Linha Hisuian Arcanine
 info(58, 'hisuian growlithe', 'fogo', 'pedra', 60).
 info(59, 'hisuian arcanine', 'fogo', 'pedra', 95).
@@ -40,21 +40,21 @@ info(92, 'gastly', 'fantasma', 'venenoso', 30).
 info(93, 'haunter', 'fantasma', 'venenoso', 45).
 info(94, 'gengar', 'fantasma', 'venenoso', 60).
 % Linha Pawmot 
-info(921, 'pawmi', 'elétrico', 'nenhum', 45).
-info(922, 'pawmo', 'elétrico', 'lutador', 60).
-info(923, 'pawmot', 'elétrico', 'lutador', 70).
+info(921, 'pawmi', 'eletrico', 'nenhum', 45).
+info(922, 'pawmo', 'eletrico', 'lutador', 60).
+info(923, 'pawmot', 'eletrico', 'lutador', 70).
 
 % Ataques do pokemon 
 %moveset(id, atk1, atk2)
-moveset(0908, 'planta mortal', 'flor explosiva').
-moveset(0124, 'confusão', 'avalanche').
-moveset(0306, 'golpe pesado', 'raio meteórico').
-moveset(0260, 'hidro canhão', 'terremoto').
-moveset(0094, 'lambida', 'bomba de lodo').
-moveset(0149, 'meteoro do dragão', 'furacão').
-moveset(0257, 'aura esférica', 'chama furacão').
-moveset(0040, 'jogo duro', 'pancada').
-moveset(0738, 'tesoura x', 'descarga').
+moveset(908, 'planta mortal', 'flor explosiva').
+moveset(124, 'confusão', 'avalanche').
+moveset(306, 'golpe pesado', 'raio meteórico').
+moveset(260, 'hidro canhão', 'terremoto').
+moveset(094, 'lambida', 'bomba de lodo').
+moveset(149, 'meteoro do dragão', 'furacão').
+moveset(257, 'aura esférica', 'chama furacão').
+moveset(40, 'jogo duro', 'pancada').
+moveset(738, 'tesoura x', 'descarga').
 
 % Esse pokemon evolui para qual?
 evolui(id_base, id_evolucao).
@@ -75,27 +75,27 @@ evolui(922, 923). % Pawmo -> Pawmot
 
 
 % Informações dos Ataques
-atkinfo(atk, tipo, dano)
-atkinfo('planta mortal', 'grass', 100).
-atkinfo('talho noturno', 'dark', 45).
-atkinfo('confusão', 'psychic', 19).
-atkinfo('avalanche', 'ice', 85).
-atkinfo('golpe pesado', 'steel', 70).
-atkinfo('raio meteórico', 'rock', 140).
-atkinfo('hidro canhão', 'water', 90).
-atkinfo('terremoto', 'ground', 140).
-atkinfo('lambida', 'ghost', 5).
-atkinfo('bomba de lodo', 'poison', 85).
-atkinfo('meteoro do dragão', 'ghost', 150).
-atkinfo('furacão', 'flying', 105).
-atkinfo('aura esférica', 'fighting', 100).
-atkinfo('chama furacão', 'flying', 13).
+%atkinfo(atk, tipo, dano)
+atkinfo('planta mortal', 'grama', 100).
+atkinfo('talho noturno', 'sombrio', 45).
+atkinfo('confusão', 'psiquico', 19).
+atkinfo('avalanche', 'gelo', 85).
+atkinfo('golpe pesado', 'aço', 70).
+atkinfo('raio meteórico', 'pedra', 140).
+atkinfo('hidro canhão', 'agua', 90).
+atkinfo('terremoto', 'terra', 140).
+atkinfo('lambida', 'fantasma', 5).
+atkinfo('bomba de lodo', 'venenoso', 85).
+atkinfo('meteoro do dragão', 'dragao', 150).
+atkinfo('furacão', 'voador', 105).
+atkinfo('aura esférica', 'Lutador', 100).
+atkinfo('chama furacão', 'voador', 13).
 atkinfo('jogo duro', 'fada', 90).
 atkinfo('pancada', 'normal', 6).
-atkinfo('tesoura x', 'bug', 45).
-atkinfo('descarga', 'electric', 65).
+atkinfo('tesoura x', 'inseto', 45).
+atkinfo('descarga', 'eletrico', 65).
 
 % Fraquezas
-%fraqueza(tipoforte, tipofraco)
+%eficacia(tipo1, tipo2, fator_multiplicacao)
 
 
